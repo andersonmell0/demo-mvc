1d760d9137bbdbe238b4ba4aa9eefbb3f9b4cb89
305050590 <anderson.mello@pm.ba.gov.br>
2020-10-20 22:46:42 -0300

    AbstractDao and Dao classes

---
+++ demo-mvc/src/main/java/com/example/demo/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao;
+
+import org.springframework.stereotype.Repository;
+import com.example.demo.domain.Departamento;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao{
+
+}
