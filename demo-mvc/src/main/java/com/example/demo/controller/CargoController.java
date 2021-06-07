a67500153b8b1029bb4bf17bd923ec6913bdf557
Anderson Mello <anderson.mello@pm.ba.gov.br>
2021-06-07 12:11:09 -0300

    crud cargos

--- demo-mvc/src/main/java/com/example/demo/controller/CargoController.java
+++ demo-mvc/src/main/java/com/example/demo/controller/CargoController.java
@@ -1,20 +1,77 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.domain.Cargo;
+import com.example.demo.domain.Departamento;
+import com.example.demo.service.CargoService;
+import com.example.demo.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 	
+	@Autowired
+	private CargoService service;
+	
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", service.buscarTodos());
 		return "/cargo/lista";
+	}
+	
+	@PostMapping("salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		service.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido");
+		return "redirect:/cargos/listar";		
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listDepartamentos(){
+		return departamentoService.buscarTodos();
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", service.buscarPorId(id));
+		return "/cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		service.editar(cargo);
+		attr.addFlashAttribute("success", "Cargo editado.");
+		return "redirect:/cargos/listar";		
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		
+		if	(service.cargoTemFuncionarios(id)) {
+			model.addAttribute("fail", "Cargo não removido. Possui Funcionário(s) vinculado(s).");
+		}else {
+			service.excluir(id);
+			model.addAttribute("success", "Cargo removido.");
+		}		
+		return listar(model);
 	}
 }
--- demo-mvc/src/main/java/com/example/demo/service/CargoServiceImpl.java
+++ demo-mvc/src/main/java/com/example/demo/service/CargoServiceImpl.java
@@ -40,4 +40,12 @@ public class CargoServiceImpl implements CargoService{
 		return dao.findAll();
 	}
 
+	@Override
+	public boolean cargoTemFuncionarios(Long id) {
+		if(buscarPorId(id).getFuncionarios().isEmpty()) {
+			return false;
+		}
+		return true;
+	}
+
 }
