719afdee044234bc89eba7edc0c49d3d59d77d59
Anderson Mello <anderson.mello@pm.ba.gov.br>
2021-06-09 15:00:21 -0300

    busca por datas

--- demo-mvc/src/main/java/com/example/demo/dao/FuncionarioDao.java
+++ demo-mvc/src/main/java/com/example/demo/dao/FuncionarioDao.java
@@ -1,11 +1,12 @@
 package com.example.demo.dao;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import com.example.demo.domain.Funcionario;
 
 public interface FuncionarioDao {		
-
+	
 	void save(Funcionario funcionario);
 	
 	void update(Funcionario funcionario);
@@ -17,4 +18,7 @@ public interface FuncionarioDao {
 	List<Funcionario> findAll();
 	List<Funcionario> findByNome(String nome);
 	List<Funcionario> findByCargo(Long id);
+	List<Funcionario> findByDataEntradaDataSaida(LocalDate entrada, LocalDate saida);
+	List<Funcionario> findByDataEntrada(LocalDate entrada);
+	List<Funcionario> findByDataSaida(LocalDate saida);
 }
--- demo-mvc/src/main/java/com/example/demo/service/FuncionarioService.java
+++ demo-mvc/src/main/java/com/example/demo/service/FuncionarioService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import com.example.demo.domain.Funcionario;
@@ -13,5 +14,6 @@ public interface FuncionarioService {
 	List<Funcionario> buscarTodos();
 	List<Funcionario> buscarPorNome(String nome);
 	List<Funcionario> buscarPorCargo(Long id);
+	List<Funcionario> buscarPorDatas(LocalDate entrada, LocalDate saida);
 
 }
